1bd2a64a7d6cc56e9d20cac7f63bccea1b399c0d
Songcoming <songcoming@gmail.com>
2016-09-11 10:36:14 +0800

    Update CryptoChalSet1_2.java

--- CryptoChalSet1_2.java
+++ CryptoChalSet1_2.java
@@ -1,14 +1,11 @@
 public class CryptoChalSet1_2{
 
-
 	static String hexString = "0123456789abcdef";
 
 	static String textHex1 = "1c0111001f010100061a024b53535009181c";
 	static String textHex2 = "686974207468652062756c6c277320657965";
 
-
 	public static char[] hexToChar(String hex){
-
 		char[] ch = new char[hex.length() / 2];
 
 		for(int i = 0; i < hex.length(); i += 2){
@@ -18,9 +15,7 @@ public class CryptoChalSet1_2{
 		return ch;
 	}
 
-
 	public static char[] charXorChar(char[] text1, char[] text2){
-
 		char[] xorResult = new char[text1.length];
 
 		for(int i = 0; i < text1.length; i++){
@@ -29,7 +24,6 @@ public class CryptoChalSet1_2{
 
 		return xorResult;
 	}
-
 
 	public static String charToHex(char[] myChar){
 		char[] resultChar = new char[textHex1.length()];
@@ -44,9 +38,7 @@ public class CryptoChalSet1_2{
 		return resultStr;
 	}
 	
-
 	public static void main(String[] args){
 		System.out.println(charToHex(charXorChar(hexToChar(textHex1),hexToChar(textHex2))));
 	}
-
 }
